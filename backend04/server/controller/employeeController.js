3894b875d638e401706a6672f80325f7d54da78e
Muhammad Rifaldi <muhammadrifaldi410@gmail.com>
2022-07-15 22:03:10 +0700

    Menambahkan Fungsi Didalam EmployeeController pada Backend04

--- backend04/server/controller/employeeController.js
+++ backend04/server/controller/employeeController.js
@@ -1,18 +1,37 @@
+const findAll = async (req,res)=>{
+	try {
+		const employee = await req.context.models.employees.findAll()
+		return res.send(employee)
+	} catch (error) {
+		return res.status(404).send(error)
+	}
+}
+
+const findOne = async (req,res)=>{
+	try {
+		const employee = await req.context.models.employees.findOne({
+			where:{employee_id : req.params.id}
+		})
+		return res.send(employee)
+	} catch (error) {
+		return res.status(404).send(error)
+	}
+}
+
 const create = async (req,res)=>{
 	const {files, fields} = req.fileAttribute
 	try {
 		const employee = await req.context.models.employees.create({
-			employee_id : parseInt(fields[0].value),
-			first_name : fields[1].value,
-			last_name : fields[2].value,
-			email : fields[3].value,
-			phone_number : fields[4].value,
+			first_name : fields[0].value,
+			last_name : fields[1].value,
+			email : fields[2].value,
+			phone_number : fields[3].value,
 			// hire_date : new Date(), Untuk menggunakan tanggal sekarang tanpa harus memasukkan inputan
-			hire_date : fields[5].value,
-			job_id : parseInt(fields[6].value), // parseInt digunakan untuk merubah string menjadi integer
-			salary : fields[7].value,
-			manager_id : parseInt(fields[8].value),
-			department_id : parseInt(fields[9].value),
+			hire_date : fields[4].value,
+			job_id : fields[5].value, // parseInt digunakan untuk merubah string menjadi integer
+			salary : fields[6].value,
+			manager_id : fields[7].value,
+			department_id : fields[8].value,
 			emp_profile : files[0].file.newFilename
 		})
 		return res.send(employee)
@@ -21,6 +40,42 @@ const create = async (req,res)=>{
 	}
 }
 
+const update = async (req,res)=>{
+	let {files, fields} = req.fileAttribute
+	try {
+		const employee = await req.context.models.employees.update({
+			first_name : fields[0].value,
+			last_name : fields[1].value,
+			email : fields[2].value,
+			phone_number : fields[3].value,
+			hire_date : fields[4].value,
+			job_id : fields[5].value,
+			salary : fields[6].value,
+			manager_id : fields[7].value,
+			department_id : fields[8].value,
+			emp_profile : files[0].file.newFilename
+		},{ returning : true , where:{employee_id : req.params.id}})
+		return res.send(employee)
+	} catch (error) {
+		return res.status(404).send(error)
+	}
+}
+
+const deleted = async (req,res)=>{
+	try {
+		const employee = await req.context.models.employees.destroy({
+			where:{employee_id : req.params.id}
+		})
+		return res.send('delete '+employee+' rows')
+	} catch (error) {
+		return res.status(404).send(error)
+	}
+}
+
 export default {
-	create
+	findAll,
+	findOne,
+	create,
+	update,
+	deleted
 }
--- backend04/server/routes/employeeRoutes.js
+++ backend04/server/routes/employeeRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import controller from "../controller/index";
 import uploadImages from "../../middleware/uploadImages";
-
+	
 const router = Router()
 
+router.get('/',controller.employeeController.findAll)
+router.get('/:id',controller.employeeController.findOne)
 router.post('/', uploadImages.uploadFiles, controller.employeeController.create)
 router.get('/file/:filename', uploadImages.showFile)
+router.put('/:id', uploadImages.uploadFiles, controller.employeeController.update)
+router.delete('/:id',controller.employeeController.deleted)
 
 export default router
